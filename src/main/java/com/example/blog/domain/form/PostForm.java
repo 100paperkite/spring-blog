c9fbe6158b79483706e6b738953dad7d60d58953
Baek Jiyeon <100.paperkite@gmail.com>
2023-03-09 22:58:29 +0900

    검증을 Spring bean validator 로 변경.

--- src/main/java/com/example/blog/domain/form/PostForm.java
+++ src/main/java/com/example/blog/domain/form/PostForm.java
@@ -1,10 +1,13 @@
 package com.example.blog.domain.form;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class PostForm {
+    @NotBlank
     private String title;
+    @NotBlank
     private String content;
 }
--- src/main/java/com/example/blog/web/PostController.java
+++ src/main/java/com/example/blog/web/PostController.java
@@ -3,14 +3,12 @@ package com.example.blog.web;
 import com.example.blog.domain.Post;
 import com.example.blog.domain.form.PostForm;
 import com.example.blog.service.PostService;
-import com.example.blog.web.validation.PostFormValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -22,13 +20,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PostController {
     private final PostService postService;
-    private final PostFormValidator postFormValidator;
-
-    @InitBinder
-    public void init(WebDataBinder dataBinder){
-        dataBinder.addValidators(postFormValidator);
-    }
-
 
     @GetMapping("/all")
     public String allPosts(Model model){
